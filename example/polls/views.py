dfe9ffd50a9eaed45f4c419e28c25dd8f147f911
vpatil0029 <102610391+vpatil0029@users.noreply.github.com>
2022-11-10 14:41:36 +0530

    test

---
+++ example/polls/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+from rest_framework import viewsets
+
+from . import models
+from . import serializers
+
+
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = models.Question.objects.all()
+    serializer_class = serializers.QuestionSerializer
